0c15c92897183291a7c1a7e629aae6251578d4fc
robertgates1928 <robertgates1928@robergates1928@protonmail.com>
2023-12-17 23:14:32 +0800

    111

--- function_app.py
+++ function_app.py
@@ -17,6 +17,7 @@ container = database.get_container_client(CONTAINER_NAME)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     logging.info('Python Cosmos DB trigger function processed a request.')
+    name = None
     try:
         req_body = req.get_json()
     except ValueError:
--- test_login.py
+++ test_login.py
@@ -2,13 +2,13 @@ import requests
 
 
 LOCAL_DEV_URL="http://localhost:7071/api/http_trigger"
-PUBLIC_URL="https://quiplashtest.azurewebsites.net/api/player/login"
-TEST_URL = LOCAL_DEV_URL
+PUBLIC_URL="https://vsharetest.azurewebsites.net/api/http_trigger"
+TEST_URL = PUBLIC_URL
 input_user = '{"name": "testname","pwd": "pwd111" }'
 
 response = requests.get(TEST_URL,data=input_user)
 
-dict_response = response.content  
+dict_response = response.raw  
 
 print(dict_response)
 
